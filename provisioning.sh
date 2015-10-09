699ae386c45b0b7162d4d1b7c45c18f30b90dc25
U-SDA\anton.herzog <anton.herzog@AHerzog-PC.sda.local>
2015-10-09 12:33:01 +0200

    Changed default nginx setup.

--- provisioning.sh
+++ provisioning.sh
@@ -5,7 +5,4 @@ apt-get install -y nginx
 rm -rf /etc/nginx/sites-enabled
 cp -r /vagrant/sites-enabled /etc/nginx
 
-rm -rf /usr/share/nginx/www
-ln -s /vagrant/www /usr/share/nginx/www
-
 service nginx start
